ff8647340e1417088b4b8a79b591dab6ee542f32
bruhchan417 <singtchan6-c@my.cityu.edu.hk>
2026-01-15 14:24:52 +0800

    Reorder import statements in app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-
-# app.py
-import io
-
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""    # don't even try to init CUDA
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # safer CPU kernels (avoid oneDNN quirks)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"   # quieter TF logs (optional)
+
+import io
 
 from typing import List, Tuple
 import threading
